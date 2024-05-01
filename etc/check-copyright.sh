8805a80f7a0a4e4c7ee9ed4a110030e2c7cb5435
Andrew Fitzgibbon <awf@graphcore.ai>
2024-05-01 20:09:57 +0100

    test

--- etc/check-copyright.sh
+++ etc/check-copyright.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-# Copyright (c) 2024 Graphcore Ltd. All rights reserved.
+# Copyright (c) 2024 Graphcore Ltd. All rights reserved.x
 
+echo "x $@" 1>&2
 PATTERN='Copyright \(c\) 202[0-9] Graphcore Ltd\. +All rights reserved\.'
 IGNORE=(docs/Makefile docs/make.bat \*.md \*requirements\*.txt)
 GITNOTMATCH=':(exclude)' # See git pathspec docs
