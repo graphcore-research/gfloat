bc44ff0dd0ae1972e7ea3e4e5ad025b76bef7c74
Andrew Fitzgibbon <awf@graphcore.ai>
2024-05-01 21:53:56 +0100

    marker commit

--- etc/check-copyright.sh
+++ etc/check-copyright.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/bash
 # Copyright (c) 2024 Graphcore Ltd. All rights reserved.
-
-echo "x $@" 1>&2
-PATTERN='Copyright \(c\) 202[0-9] Graphcore Ltd\. +All rights reserved\.'
-IGNORE=(docs/Makefile docs/make.bat \*.md \*requirements\*.txt)
-GITNOTMATCH=':(exclude)' # See git pathspec docs
-git ls-files -- . ${IGNORE[@]/#/$GITNOTMATCH}  | xargs grep -L -E "$PATTERN"
