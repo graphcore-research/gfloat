67128044a847135d864a558e1c81ad10c471a775
Andrew Fitzgibbon <awf@graphcore.ai>
2024-05-10 11:30:24 +0100

    Comment, remove debug prints

--- test/test_microxcaling.py
+++ test/test_microxcaling.py
@@ -40,8 +40,10 @@ def test_mx(
     mx_etype: ElemFormat,
     gf_etype: FormatInfo,
 ) -> None:
+    # Input tensor
     A = torch.arange(32) / 2 - 5
 
+    # Compute MX quantization
     mx_specs = dict(
         block_size=32,
         scale_bits=8,
@@ -52,28 +54,17 @@ def test_mx(
 
     mx_dq = quantize_mx_op(A, mx_specs, mx_etype, axes=0, round=mx_round)
 
+    # Compute GFloat quantization
     fi = BlockFormatInfo("test", gf_etype, 32, format_info_ocp_e8m0)
 
+    # Compute scale - this is not considered GFloat's job
     amax = A.abs().max()
     q_log2scale = torch.floor(torch.log2(amax)).item() - fi.etype.emax
     q_scale = 2**q_log2scale
 
-    print(f"{q_scale=}")
-
-    enc = list(encode_block(fi, q_scale, (a.item() for a in A), gf_round))
-    print(f"{enc=}")
-    print("decoded_scale=", decode_float(fi.stype, enc[0]).fval)
-    print("decoded_vals=", list(decode_float(fi.etype, e).fval for e in enc[1:]))
-    print(
-        "all_vals=",
-        *(
-            str(decode_float(fi.etype, i).fval) + ("" if i & 1 else "e")
-            for i in range(fi.etype.code_of_max + 1)
-        ),
-    )
+    # Apply scale to encode and decode
+    enc = encode_block(fi, q_scale, (a.item() for a in A), gf_round)
     gf_dq = list(decode_block(fi, enc))
-    print("input=", *(str(v.item()) for v in A))
-    print("mx_dq=", *(str(v.item()) for v in mx_dq))
-    print("gf_dq=", *(str(v) for v in gf_dq))
 
+    # Compare
     np.testing.assert_allclose(gf_dq, mx_dq)
